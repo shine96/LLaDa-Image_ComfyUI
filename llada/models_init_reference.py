929a37bf74bf0be4593e85a3d07625b00b426277
pipi <10670374@qq.com>
2026-09-06 08:51:03 +0800

    chore(pipeline): 移除 pipeline_llada_image.py 文件及相关内容
    - 完全删除了 llada/pipeline_llada_image.py 文件
    - 清理了 llada/models_init_reference.py 中相关导入内容
    - 合并清除代码中的冲突标记及无效内容
    - 更新 README.md 中模型权重和仓库链接地址
    - 还原 LLaDA-Image-Turbo 和 LLaDA-Image-Turbo-EDIT JSON 文件至合并前状态

--- llada/models_init_reference.py
+++ llada/models_init_reference.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from .transformer_llada_image import (
     LLaDAImageQueryFormerModel,
     LLaDAImageSigVQModel,
@@ -12,18 +11,3 @@ __all__ = [
     "LLaDAImageTextProjectionModel",
     "LLaDAImageTransformer2DModel",
 ]
-=======
-from .transformer_llada_image import (
-    LLaDAImageQueryFormerModel,
-    LLaDAImageSigVQModel,
-    LLaDAImageTextProjectionModel,
-    LLaDAImageTransformer2DModel,
-)
-
-__all__ = [
-    "LLaDAImageQueryFormerModel",
-    "LLaDAImageSigVQModel",
-    "LLaDAImageTextProjectionModel",
-    "LLaDAImageTransformer2DModel",
-]
->>>>>>> 12597edf68b0e5d27842a1a970a769692e63aca9
--- llada/pipelines_init_reference.py
+++ llada/pipelines_init_reference.py
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 from .pipeline_llada_image import LLaDAImagePipeline
 from .pipeline_output import LLaDAImagePipelineOutput
 
 __all__ = ["LLaDAImagePipeline", "LLaDAImagePipelineOutput"]
-=======
-from .pipeline_llada_image import LLaDAImagePipeline
-from .pipeline_output import LLaDAImagePipelineOutput
-
-__all__ = ["LLaDAImagePipeline", "LLaDAImagePipelineOutput"]
->>>>>>> 12597edf68b0e5d27842a1a970a769692e63aca9
